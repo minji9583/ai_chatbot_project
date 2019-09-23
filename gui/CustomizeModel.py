4ea0ccbc1b9cf0e55894f49c0e06d7a2b5dde92f
Park Sangho <sxngho@naver.com>
2019-09-23 19:17:02 +0900

    [#6] S1P2243009-47 MainTitleBar 구현

--- gui/CustomizeModel.py
+++ gui/CustomizeModel.py
@@ -1,10 +1,14 @@
 import sys
+import os
 import matplotlib.pyplot as plt
 import pickle
 from PyQt5.QtWidgets import \
     QApplication, QWidget, QDesktopWidget, QGridLayout, QLabel, QPushButton, \
     QFileDialog, QLineEdit, QMessageBox, QBoxLayout, QGroupBox
-from PyQt5 import QtWidgets, QtCore
+from PyQt5 import QtWidgets, QtCore, QtGui
+
+ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
+
 
 
 class MyApp(QWidget):
@@ -12,11 +16,11 @@ class MyApp(QWidget):
     css = """
         QWidget {
             color: #FFF;
-            background: #4D342A;
+            background: #FFF;
         }
         QWidget#windowTitle {
             color: #FFFFFF;
-            background: #FFFFFF;
+            background: #FFF;
         }
         QWidget#windowTitle QLabel {
             color: #000000;
@@ -50,12 +54,9 @@ class MyApp(QWidget):
         self.step1 = QGroupBox("step1")
             
         self.setWindowFlags(QtCore.Qt.FramelessWindowHint) #프레임을 없애고
-        self.setStyleSheet(self.css) # css를 적용함
-        titlebar_widget = QtWidgets.QWidget()
+        #self.setStyleSheet(self.css) # css를 적용함
+        titlebar_widget =  MainTitleBar(self)
         titlebar_widget.setObjectName("windowTitle")
-        title_label = QtWidgets.QLabel("학습 장치")
-        title_hbox = QtWidgets.QHBoxLayout(titlebar_widget)
-        title_hbox.addWidget(title_label)
         
         self.container.addWidget(titlebar_widget)
         self.container.addWidget(self.step1)
@@ -99,18 +100,17 @@ class MyApp(QWidget):
     def setMoreUI(self):
         # 이전 내용을 모두 지우고 프로그래스바를 띄운다
         self.step1.deleteLater();
-
-
+        
         #여기에 프로그래스바가 생겨야하고... 끝나면 더 늘리기
         #프로그래스바는 나중에 했다치고, 프로그레스바가 끝나면 아래 버튼 이벤트가 실행되게
         self.step2 = QGroupBox("step2")
         self.container.addWidget(self.step2)
         self.grid = QGridLayout()
         self.step2.setLayout(self.grid)
-
+        self.container.addStretch()
         self.tmpbtn = QPushButton("프로그레스바 컴플릿!", self);
         self.tmpbtn.clicked.connect(self.setFianlUI)
-        self.grid.addWidget(self.tmpbtn, 1, 1)
+        self.grid.addWidget(self.tmpbtn, 0, 1)
 
     def setFianlUI(self):
         self.resize(600, 800)
@@ -150,8 +150,96 @@ class MyApp(QWidget):
         self.move(qr.topLeft())
 
 
+class MainTitleBar(QtWidgets.QWidget):
+    """제목 표시줄 위젯"""
+    qss = """
+        QWidget {
+            color: #FFFFFF;
+            background: #4D342A;
+            height: 32px;
+        }
+        QLabel {
+            color: #FFFFFF;
+            background: #4D342A;
+            font-size: 16px;
+            padding: 5px 5px;
+        }
+        QToolButton {
+            background: #4D342A;
+            border: none;
+        }
+        QToolButton:hover{
+            background: #5E453B;
+        }
+    """
 
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.bar_height = 36
+        self.parent = parent
+        self.has_clicked = False
+        self.is_maximized = False
+        self.setStyleSheet(self.qss)
+        layout = QtWidgets.QHBoxLayout(self)
+        layout.setContentsMargins(0, 0, 0, 0)
+        layout.setSpacing(0)
 
+        label = QtWidgets.QLabel("학습 장치")
+        label.setFixedHeight(self.bar_height)
+        btn_minimize = self.create_tool_btn('minimize_w.png')
+        btn_minimize.clicked.connect(self.show_minimized)
+        btn_close = self.create_tool_btn('close_w.png')
+        btn_close.clicked.connect(self.close)
+
+        layout.addWidget(label)
+        layout.addWidget(btn_minimize)
+        layout.addWidget(btn_close)
+
+    def create_tool_btn(self, icon_path):
+        """제목표시줄 아이콘 생성"""
+        icon = os.path.join(ROOT_PATH, 'img', icon_path)
+        print(ROOT_PATH)
+        btn = QtWidgets.QToolButton(self)
+        btn.setIcon(QtGui.QIcon(icon))
+        btn.setIconSize(QtCore.QSize(self.bar_height, self.bar_height))
+        btn.setFixedSize(self.bar_height, self.bar_height)
+        return btn
+
+    def show_minimized(self):
+        """버튼 명령: 최소화"""
+        self.parent.showMinimized()
+
+    def close(self):
+        """버튼 명령: 닫기"""
+        self.parent.close()
+
+    def mousePressEvent(self, event):
+        """오버로딩: 마우스 클릭 이벤트
+        - 제목 표시줄 클릭시 이동 가능 플래그
+        """
+        if event.button() == QtCore.Qt.LeftButton:
+            self.parent.is_moving = True
+            self.parent.offset = event.pos()
+
+    def mouseMoveEvent(self, event):
+        """오버로딩: 마우스 이동 이벤트
+        - 제목 표시줄 드래그시 창 이동
+        """
+        if self.parent.is_moving:
+            self.parent.move(event.globalPos() - self.parent.offset)
+    '''
+    def mouseDoubleClickEvent(self, event):
+        """오버로딩: 더블클릭 이벤트
+        - 제목 표시줄 더블클릭시 최대화
+        """
+        if self.is_maximized:
+            self.parent.showNormal()
+            self.is_maximized = False
+        else:
+            self.parent.showMaximized()
+            self.is_maximized = True
+
+    '''
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = MyApp()
