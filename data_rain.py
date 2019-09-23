6c59ec0b945661b0cc13c86b267a0d0ffe0f8a4b
sweetrain096 <gpfhddl09@gmail.com>
2019-09-23 13:53:32 +0900

    [#4] S1P2243009-36 data_out폴더 데이터 생성
    1-1~1-3 data.py 수정하고 data_out 수정

--- data_rain.py
+++ data_rain.py
@@ -23,7 +23,9 @@ MARKER = [PAD, STD, END, UNK]
 
 
 # Req 1-1-1. 데이터를 읽고 트레이닝 셋과 테스트 셋으로 분리
-def load_data(Q, A):
+def load_data():
+    data_df = pd.read_csv(DEFINES.data_path)
+    Q, A = list(data_df['Q']), list(data_df['A'])
     train_q, test_q, train_a, test_a = train_test_split(Q, A)
     return train_q, train_a, test_q, test_a
 
@@ -40,19 +42,10 @@ def prepro_noise_canceling(data):
 
 # Req 1-1-3. 텍스트 데이터에 토크나이징
 def tokenizing_data(data):
-    dictionary = dict()
-    index = 1
-    data = data[:]
+    token_data = []
     for i in range(len(data)):
-        data[i] = data[i].split()
-        for word in data[i]:
-            if not dictionary.get(word):
-                dictionary[word] = index
-                index += 1
-    # print(data)
-    # print(dictionary)
-
-    return dictionary
+        token_data.extend(data[i].split())
+    return token_data
 
 # Req 1-2-1. 토큰화된 트레이닝 데이터를 인코더에 활용할 수 있도록 전 처리
 def enc_processing(value, dictionary):
@@ -68,7 +61,8 @@ def enc_processing(value, dictionary):
 
         # 하나의 seq에 index를 저장할 배열 초기화
         seq_index = []
-        for word in seq.split():
+        words = tokenizing_data(seq)
+        for word in words:
             if dictionary.get(word):
                 # seq_index에 dictionary 안의 인덱스를 extend 한다
                 seq_index.extend([dictionary.get(word)])
@@ -229,7 +223,7 @@ def eval_input_fn(eval_input_enc, eval_input_dec, eval_target_dec, batch_size):
     return iterator.get_next()
     
 # Req 1-3-1. 단어 사전 파일 vocabularyData.voc를 생성하고 단어와 인덱스 관계를 출력
-def load_voc(filename):
+def load_voc():
     # 사전을 담을 배열 준비한다.
     voc_list = []
     # 사전을 구성한 후 파일로 저장 진행한다.
@@ -240,7 +234,7 @@ def load_voc(filename):
         # 그래서 데이터가 존재 하면 사전을 만들기 위해서
         # 데이터 파일의 존재 유무를 확인한다.
 
-        data_df = pd.read_csv(filename)
+        data_df = pd.read_csv(DEFINES.data_path)
         # 판다스의 데이터 프레임을 통해
         # 질문과 답에 대한 열을 가져 온다.
         question, answer = data_df['Q'], data_df['A']
@@ -251,9 +245,8 @@ def load_voc(filename):
         data.extend(answer)
 
         # data를 토크나이즈하여 words에 저장한다.
-        words = []
-        for i in data:
-            words.extend(i.split())
+        words = tokenizing_data(data)
+
         # 중복되는 단어(토큰)를 제거
         words = list(set(words))
 
@@ -278,7 +271,6 @@ def load_voc(filename):
 
 # Req 1-3-2. 사전 리스트를 받아 인덱스와 토큰의 dictionary를 생성
 def make_voc(voc_list):
-    print(voc_list)
     char2idx, idx2char = dict(), dict()
     for i in range(len(voc_list)):
         word = voc_list[i]
@@ -296,18 +288,18 @@ def pred_next_string(value, dictionary):
 
 def main(self):
     char2idx, idx2char, voc_length = load_voc()
-
+    train_q, train_a, test_q, test_a = load_data()
+    enc_processing(train_q, char2idx)
+    print(pred_next_string([4, 62, 473, 4423, 2354, 2, 46], idx2char))
 
 if __name__ == '__main__':
     tf.logging.set_verbosity(tf.logging.INFO)
     tf.app.run(main)
 
 
-# train_q, train_a, test_q, test_a = load_data('data_in/ChatBotData.csv')
+
 # prepro_train_q = prepro_noise_canceling(train_q)
 # dictionary = tokenizing_data(prepro_train_q)
 # enc_train_q = enc_processing(train_q, dictionary)
 # dec_train_a = dec_input_processing(train_a, dictionary)
 # dec_target_processing(train_a, dictionary)
-char2idx, idx2char, len_voc_list = load_voc('data_in/ChatBotData.csv')
-print(pred_next_string([4, 62, 473, 4423, 2354, 2, 46], idx2char))
