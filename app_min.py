a298b8bad9036d432e9301b2bb64637865d7668f
min <smp1991@naver.com>
2019-09-30 17:42:06 +0900

    [#2] app_min.py에 SLACK_TOKEN, SLACK_SIGNING_SECRET 등 slack 연동 정보 입력

--- app_min.py
+++ app_min.py
@@ -16,10 +16,8 @@ from slackeventsapi import SlackEventAdapter
 
 
 # slack 연동 정보 입력 부분
-SLACK_TOKEN = os.getenv('SLACK_TOKEN')
-SLACK_SIGNING_SECRET = os.getenv('SLACK_SIGNING_SECRET')
-SLACK_TOKEN = "xoxb-718907786578-720177174562-oKkmHrXJhhDyxatiVMO3A9pK"
-SLACK_SIGNING_SECRET = "fa3d6193e36d26163abc90a4507ceec8"
+SLACK_TOKEN = 'xoxb-718907786578-720177174562-1qx9BjbVbLkrIB9yfikj8fVr'
+SLACK_SIGNING_SECRET = 'fa3d6193e36d26163abc90a4507ceec8'
 
 app = Flask(__name__)
 
@@ -28,67 +26,14 @@ slack_web_client = WebClient(token=SLACK_TOKEN)
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
+
 # Req. 2-2-1 대답 예측 함수 구현
-def predict(sentence_data):
+def predict():
 
-    # 데이터를 통한 사전 구성 한다.
-    char2idx, idx2char, vocabulary_length = data.load_voc()
-
-    # 테스트용 데이터 만드는 부분이다.
-    # 인코딩 부분 만든다.
-    input = ""
-    for i in sys.argv[1:]:
-        input += i
-        input += " "
-
-    print(input)
-    predic_input_enc = None
-    # 학습 과정이 아니므로 디코딩 입력은
-    # 존재하지 않는다.(구조를 맞추기 위해 넣는다.)
-    predic_output_dec = None
-    # 학습 과정이 아니므로 디코딩 출력 부분도
-    # 존재하지 않는다.(구조를 맞추기 위해 넣는다.)
-    predic_target_dec = None
-
-    # 에스티메이터 구성한다.
-    classifier = tf.estimator.Estimator(
-        model_fn=ml.Model,  # 모델 등록한다.
-        model_dir=DEFINES.check_point_path,  # 체크포인트 위치 등록한다.
-        params={  # 모델 쪽으로 파라메터 전달한다.
-            'embedding_size': DEFINES.embedding_size,
-            'model_hidden_size': DEFINES.model_hidden_size,  # 가중치 크기 설정한다.
-            'ffn_hidden_size': DEFINES.ffn_hidden_size,
-            'attention_head_size': DEFINES.attention_head_size,
-            'learning_rate': DEFINES.learning_rate,  # 학습율 설정한다.
-            'vocabulary_length': vocabulary_length,  # 딕셔너리 크기를 설정한다.
-            'embedding_size': DEFINES.embedding_size,  # 임베딩 크기를 설정한다.
-            'layer_size': DEFINES.layer_size,
-            'max_sequence_length': DEFINES.max_sequence_length,
-            'xavier_initializer': DEFINES.xavier_initializer
-        })
-
-
-    # 예측을 하는 부분이다.
-    predictions = classifier.predict(input_fn=lambda: data.eval_input_fn(predic_input_enc, predic_input_dec, predic_target_dec, 1))
-
-    answer, finished = None
-
-
-
-    return answer
+    return None
 
 # Req 2-2-2. app.db 를 연동하여 웹에서 주고받는 데이터를 DB로 저장
-def insert(text):
-    # app.db 파일을 연결
-    conn = sqlite3.connect('app.db')
-    # 사용할 수 있도록(?) 설정
-    c = conn.cursor()
-    # 쿼리문
-    c.execute('INSERT INTO search_history(query) VALUES(?)', (text,))
-    # 저장
-    conn.commit()
-    # app.db 파일 연결 해제
-    conn.close()
+
 
 # 챗봇이 멘션을 받았을 경우
 @slack_events_adaptor.on("app_mention")
